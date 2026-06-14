61225447da7924cb382472ed64461a71e477f12f
Lucas-haddy <lucas-hadfield@live.nmit.ac.nz>
2026-06-15 09:27:36 +1200

    created three starting validation checks; validateHikerName, validateHutId & validatePartySize

--- App/inputValidator.js
+++ App/inputValidator.js
@@ -1,6 +1,26 @@
-const readline = require('node:readline');
+const validateHikerName = (name) => {
+    if (typeof name !== 'string' || name.trim() === '') {
+        console.log('Hiker name cannot be empty.');
+        return false;
+    } else {
+        return true;
+    }
+}
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const validateHutId = (hutId) => {
+    if (typeof hutId !== 'string' || hutId.trim() === '') {
+        console.log('Please enter a valid hut ID.')
+        return false;
+    } else {
+        return true;
+    }
+}
+
+const validatePartySize = (partySize) => {
+    if (!Number.isInteger(partySize) || partySize <= 0 || partySize > 20) {
+        console.log('Party size must be a positive whole number between 1 and 20.');
+        return false;
+    } else {
+        return true;
+    }
+}
