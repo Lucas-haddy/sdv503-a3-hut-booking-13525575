667eea1e5b3295b77d3390c56b008c7b7eb90219
Lucas-haddy <lucas-hadfield@live.nmit.ac.nz>
2026-06-15 11:27:07 +1200

    created readline interface for main.js file

--- App/main.js
+++ App/main.js
@@ -1,2 +1,17 @@
+const readline = require('readline')
+
 const validator = require('./inputValidator');
 const { loadData } = require('./dataStorage');
+const bookingSystem = require('./bookingSystem');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const askQuestion = (query) => {
+    return new Promise((resolve) => rl.question(query, resolve));
+};
+
+let huts = [];
+let bookings = [];
